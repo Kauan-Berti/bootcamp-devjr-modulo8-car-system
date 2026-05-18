816a3e13c0ce673b23d620c301f5fa6df3a44dc7
Kauan Berti <kauan.g.berti@gmail.com>
2026-05-18 02:31:24 -0300

    feat: created footer.component closed #8

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { TitleComponent } from "./components/title/title.component";
 import { CarComponent } from "./components/car/car.component";
 import { CarsComponent } from "./components/cars/cars.component";
+import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TitleComponent, CarComponent, CarsComponent, FormsModule],
+  imports: [RouterOutlet, TitleComponent, CarComponent, CarsComponent, FormsModule, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
