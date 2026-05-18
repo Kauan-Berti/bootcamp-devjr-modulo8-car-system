074bce909b81691550aa5f334edae2b167efb515
Kauan Berti <kauan.g.berti@gmail.com>
2026-05-17 22:21:28 -0300

    feat: created title.component closed #4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleComponent } from "./components/title/title.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('bootcamp-devjr-modulo8-car-system');
+
 }
